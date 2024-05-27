484d2e02ba7a38f0a918180ebb077d65a4167832
Antipas Zemba <169179206+AntipasZemba@users.noreply.github.com>
2024-05-27 16:15:32 -0400

    Add files via upload
    initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstOrderDisplay = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radDelivery = New System.Windows.Forms.RadioButton()
+        Me.radPickUp = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBags = New System.Windows.Forms.TextBox()
+        Me.cboProduct = New System.Windows.Forms.ComboBox()
+        Me.txtZipCode = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnAddProduct = New System.Windows.Forms.Button()
+        Me.btnTotalOrder = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lstOrderDisplay
+        '
+        Me.lstOrderDisplay.FormattingEnabled = True
+        Me.lstOrderDisplay.ItemHeight = 20
+        Me.lstOrderDisplay.Location = New System.Drawing.Point(522, 43)
+        Me.lstOrderDisplay.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.lstOrderDisplay.Name = "lstOrderDisplay"
+        Me.lstOrderDisplay.Size = New System.Drawing.Size(444, 444)
+        Me.lstOrderDisplay.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radDelivery)
+        Me.GroupBox1.Controls.Add(Me.radPickUp)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtBags)
+        Me.GroupBox1.Controls.Add(Me.cboProduct)
+        Me.GroupBox1.Controls.Add(Me.txtZipCode)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Location = New System.Drawing.Point(34, 29)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(465, 458)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Customer Info"
+        '
+        'radDelivery
+        '
+        Me.radDelivery.AutoSize = True
+        Me.radDelivery.Checked = True
+        Me.radDelivery.Location = New System.Drawing.Point(250, 302)
+        Me.radDelivery.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.radDelivery.Name = "radDelivery"
+        Me.radDelivery.Size = New System.Drawing.Size(89, 24)
+        Me.radDelivery.TabIndex = 9
+        Me.radDelivery.TabStop = True
+        Me.radDelivery.Text = "Delivery"
+        Me.radDelivery.UseVisualStyleBackColor = True
+        '
+        'radPickUp
+        '
+        Me.radPickUp.AutoSize = True
+        Me.radPickUp.Location = New System.Drawing.Point(250, 262)
+        Me.radPickUp.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.radPickUp.Name = "radPickUp"
+        Me.radPickUp.Size = New System.Drawing.Size(88, 24)
+        Me.radPickUp.TabIndex = 8
+        Me.radPickUp.TabStop = True
+        Me.radPickUp.Text = "Pick Up"
+        Me.radPickUp.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 202)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(128, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Number of Bags:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(54, 149)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Mulch Type:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(54, 94)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Zip Code:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 42)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Name:"
+        '
+        'txtBags
+        '
+        Me.txtBags.Location = New System.Drawing.Point(250, 198)
+        Me.txtBags.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtBags.Name = "txtBags"
+        Me.txtBags.Size = New System.Drawing.Size(49, 26)
+        Me.txtBags.TabIndex = 3
+        '
+        'cboProduct
+        '
+        Me.cboProduct.FormattingEnabled = True
+        Me.cboProduct.Items.AddRange(New Object() {"Black Shredded - $7.90", "Black Chips - $7.00", "Brown Shredded - $7.90", "Brown Chips - $7.00", "Red Shredded - $7.90", "Red Chips - $7.00"})
+        Me.cboProduct.Location = New System.Drawing.Point(250, 146)
+        Me.cboProduct.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.cboProduct.Name = "cboProduct"
+        Me.cboProduct.Size = New System.Drawing.Size(205, 28)
+        Me.cboProduct.TabIndex = 2
+        '
+        'txtZipCode
+        '
+        Me.txtZipCode.Location = New System.Drawing.Point(250, 91)
+        Me.txtZipCode.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtZipCode.Name = "txtZipCode"
+        Me.txtZipCode.Size = New System.Drawing.Size(98, 26)
+        Me.txtZipCode.TabIndex = 1
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(250, 38)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(122, 26)
+        Me.txtName.TabIndex = 0
+        '
+        'btnAddProduct
+        '
+        Me.btnAddProduct.Location = New System.Drawing.Point(132, 532)
+        Me.btnAddProduct.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnAddProduct.Name = "btnAddProduct"
+        Me.btnAddProduct.Size = New System.Drawing.Size(156, 45)
+        Me.btnAddProduct.TabIndex = 2
+        Me.btnAddProduct.Text = "Add Product"
+        Me.btnAddProduct.UseVisualStyleBackColor = True
+        '
+        'btnTotalOrder
+        '
+        Me.btnTotalOrder.Location = New System.Drawing.Point(336, 532)
+        Me.btnTotalOrder.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnTotalOrder.Name = "btnTotalOrder"
+        Me.btnTotalOrder.Size = New System.Drawing.Size(153, 48)
+        Me.btnTotalOrder.TabIndex = 3
+        Me.btnTotalOrder.Text = "Total Order"
+        Me.btnTotalOrder.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(536, 535)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(153, 43)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(736, 535)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(153, 43)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1006, 631)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnTotalOrder)
+        Me.Controls.Add(Me.btnAddProduct)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lstOrderDisplay)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "VB Landscape Supplies"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstOrderDisplay As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents radDelivery As RadioButton
+    Friend WithEvents radPickUp As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBags As TextBox
+    Friend WithEvents cboProduct As ComboBox
+    Friend WithEvents txtZipCode As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnAddProduct As Button
+    Friend WithEvents btnTotalOrder As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,216 @@
+﻿Public Class Form1
+    Dim strZipCode As String
+    Dim dblOrderSubTotal As Double
+    Dim blnFirstProduct As Boolean = True
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        blnFirstProduct = True
+        txtName.Enabled = True
+        txtZipCode.Enabled = True
+        radDelivery.Enabled = True
+        radPickUp.Enabled = True
+        cboProduct.Enabled = True
+        txtBags.Enabled = True
+        btnAddProduct.Enabled = True
+
+        txtName.Clear()
+        txtZipCode.Clear()
+        radDelivery.Checked = True
+
+        lstOrderDisplay.Items.Clear()
+
+        dblOrderSubTotal = 0
+
+        txtName.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub btnAddProduct_Click(sender As Object, e As EventArgs) Handles btnAddProduct.Click
+        Dim shtBags As Short
+        Dim strProduct As String
+        Dim dblProductTotal As Double
+
+        Dim blnValidated As Boolean = True
+
+        GetandValidate_Input(shtBags, strProduct, blnValidated)
+
+        If blnValidated Then
+            dblProductTotal = Calculate_Product_SubTotal(shtBags, strProduct)
+            Accumulate_Order_Total(dblProductTotal)
+            Display_Product(strProduct, shtBags, dblProductTotal)
+        End If
+    End Sub
+
+    Private Sub GetandValidate_Input(ByRef shtBags As Short, ByRef strProduct As String, ByRef blnValidated As Boolean)
+        ValidateName(blnValidated)
+        If blnValidated Then
+            ValidateZipCode(blnValidated)
+            If blnValidated Then
+                ValidateProduct(strProduct, blnValidated)
+                If blnValidated Then
+                    ValidateBags(shtBags, blnValidated)
+                End If
+            End If
+        End If
+    End Sub
+    Private Sub ValidateName(ByRef blnValidated As Boolean)
+
+        If txtName.Text = "" Then
+            MessageBox.Show("Name is Required")
+            txtName.Focus()
+            blnValidated = False
+        End If
+    End Sub
+
+    Private Sub ValidateZipCode(ByRef blnValidated As Boolean)
+        If txtZipCode.Text = "" Then
+            MessageBox.Show("Zip Code is Required")
+            txtZipCode.Focus()
+            blnValidated = False
+        Else
+            strZipCode = txtZipCode.Text
+            If radDelivery.Checked Then
+                If (strZipCode = "41042") Or (strZipCode = "41022") Or (strZipCode = "41091 ") Then
+                Else
+                    MessageBox.Show("VB Landscape only delivers to 41042, 41022, or 41091 Zip Codes")
+                    txtZipCode.Focus()
+                    blnValidated = False
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub ValidateBags(ByRef shtBags As Short, ByRef blnValidated As Boolean)
+        If Short.TryParse(txtBags.Text, shtBags) Then
+            If (shtBags > 0) And (shtBags < 51) Then
+            Else
+                MessageBox.Show("Number of Bags Must be between 1 and 50")
+                txtZipCode.Focus()
+                blnValidated = False
+            End If
+        Else
+            MessageBox.Show("Number of Bags is Required and Must be Numeric")
+            txtBags.Focus()
+            blnValidated = False
+        End If
+    End Sub
+
+    Private Sub ValidateProduct(ByRef strProduct As String, ByRef blnValidated As Boolean)
+        If cboProduct.Text = "" Then
+            MessageBox.Show("Product Must be Selected")
+            cboProduct.Focus()
+            blnValidated = False
+        Else
+            If cboProduct.Text = "Black Shredded - $7.90" Then
+                strProduct = "Black Shredded"
+            Else
+                If cboProduct.Text = "Black Chips - $7.00" Then
+                    strProduct = "Black Chips"
+                Else
+                    If cboProduct.Text = "Brown Shredded - $7.90" Then
+                        strProduct = "Brown Shredded"
+                    Else
+                        If cboProduct.Text = "Brown Chips - $7.00" Then
+                            strProduct = "Brown Chips"
+                        Else
+                            If cboProduct.Text = "Red Shredded - $7.90" Then
+                                strProduct = "Red Shredded"
+                            Else
+                                If cboProduct.Text = "Red Chips - $7.00" Then
+                                    strProduct = "Red Chips"
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Function Calculate_Product_SubTotal(ByVal shtBags As Short, ByVal strProduct As String) As Double
+        Dim dblProductCost As Double
+
+        If (strProduct = "Black Chips") Or (strProduct = "Brown Chips") Or (strProduct = "Red Chips") Then
+            dblProductCost = 7.0
+        Else
+            dblProductCost = 7.9
+        End If
+
+        Return dblProductCost * shtBags
+    End Function
+
+    Private Sub Accumulate_Order_Total(ByVal dblProductTotal As Double)
+        dblOrderSubTotal += dblProductTotal
+    End Sub
+
+    Private Sub Display_Product(ByVal strProduct As String, ByVal shtBags As String, ByVal dblProductTotal As Double)
+        If blnFirstProduct Then
+            lstOrderDisplay.Items.Add("Mulch Type" & vbTab & "Bags" & vbTab & "Total")
+            lstOrderDisplay.Items.Add(" ")
+
+            blnFirstProduct = False
+            txtName.Enabled = False
+            txtZipCode.Enabled = False
+            radDelivery.Enabled = False
+            radPickUp.Enabled = False
+        End If
+
+        lstOrderDisplay.Items.Add(strProduct & vbTab & shtBags & vbTab & dblProductTotal.ToString("c"))
+        cboProduct.ResetText()
+
+        txtBags.Clear()
+    End Sub
+
+    Private Sub btnTotalOrder_Click(sender As Object, e As EventArgs) Handles btnTotalOrder.Click
+        Dim dblTax As Double
+        Dim dblDeliveryCost As Double
+        Dim dblTotalOrderCost As Double
+
+        dblTax = Calculate_Tax()
+        dblDeliveryCost = Calculate_Delivery()
+        dblTotalOrderCost = Calculate_TotalOrderCost(dblTax, dblDeliveryCost)
+
+        Display_OrderTotals(dblTax, dblDeliveryCost, dblTotalOrderCost)
+    End Sub
+
+    Private Function Calculate_Tax() As Double
+        Return dblOrderSubTotal * 0.07
+    End Function
+
+
+    Private Function Calculate_Delivery() As Double
+        Dim dblDeliveryCost As Double
+
+        If strZipCode = "41042" Then
+            dblDeliveryCost = 10
+        Else
+            If strZipCode = "41022" Then
+                dblDeliveryCost = 12
+            Else
+                dblDeliveryCost = 15
+            End If
+        End If
+        Return dblDeliveryCost
+    End Function
+
+    Private Function Calculate_TotalOrderCost(ByVal dblTax As Double, ByVal dblDeliveryCost As Double) As Double
+        Return dblOrderSubTotal + dblTax + dblDeliveryCost
+    End Function
+
+    Private Sub Display_OrderTotals(ByVal dblTax As Double, ByVal dblDeliveryCost As Double, ByVal dblTotalOrderCost As Double)
+        lstOrderDisplay.Items.Add("================================================")
+        lstOrderDisplay.Items.Add("Total for all bags:" & vbTab & dblOrderSubTotal.ToString("c"))
+        lstOrderDisplay.Items.Add("Tax:" & vbTab & vbTab & dblTax.ToString("c"))
+        lstOrderDisplay.Items.Add("Delivery:" & vbTab & vbTab & dblDeliveryCost.ToString("c"))
+        lstOrderDisplay.Items.Add(" ")
+        lstOrderDisplay.Items.Add("Order Total:" & vbTab & dblTotalOrderCost.ToString("c"))
+
+        cboProduct.Enabled = False
+        txtBags.Enabled = False
+        btnAddProduct.Enabled = False
+    End Sub
+
+End Class
